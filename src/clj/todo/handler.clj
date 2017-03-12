68c79300d16373d8b844b316b802e1bb92b921db
Anonymous Applicant <anonymous@example.com>
2017-03-12 16:24:05 -0400

    starting new project

---
+++ src/clj/todo/handler.clj
@@ -0,0 +1,27 @@
+(ns todo.handler
+  (:require [compojure.core :refer [routes wrap-routes]]
+            [todo.layout :refer [error-page]]
+            [todo.routes.home :refer [home-routes]]
+            [todo.routes.services :refer [service-routes]]
+            [compojure.route :as route]
+            [todo.env :refer [defaults]]
+            [mount.core :as mount]
+            [todo.middleware :as middleware]))
+
+(mount/defstate init-app
+                :start ((or (:init defaults) identity))
+                :stop  ((or (:stop defaults) identity)))
+
+(def app-routes
+  (routes
+    (-> #'home-routes
+        (wrap-routes middleware/wrap-csrf)
+        (wrap-routes middleware/wrap-formats))
+    #'service-routes
+    (route/not-found
+      (:body
+        (error-page {:status 404
+                     :title "page not found"})))))
+
+
+(defn app [] (middleware/wrap-base #'app-routes))
